79ba85a27768d49c72a42182eaf05a23dc0f9127
ckr8305 <ckr8305@naver.com>
2024-10-16 09:31:46 +0900

    five

--- src/World.java
+++ src/World.java
@@ -2,5 +2,8 @@ public class World {
     public static void run() {
         System.out.println("202114051");
       }
+      public static void run2() {
+        System.out.println("hello world");
+      }
     
 }
